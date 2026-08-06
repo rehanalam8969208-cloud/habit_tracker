cf724571d94d071e0f95cbb6ad5f3b135cb96dec
rehanalam8969208-cloud <rehanalam8969208@gmail.com>
2026-08-06 07:58:14 +0530

    Add _buildTodoList method to display todos

--- lib/main.dart
+++ lib/main.dart
@@ -469,3 +469,16 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
       },
     );
   }
+
+  Widget _buildTodoList() {
+    if (todos.isEmpty) return const Center(child: Text("No To-Do items.", style: TextStyle(color: Colors.grey)));
+    return ListView.builder(
+      padding: const EdgeInsets.only(left: 16, right: 16, bottom: 100),
+      itemCount: todos.length,
+      itemBuilder: (context, index) {
+        final todo = todos[index];
+        final bool isDone = todo['isDone'] ?? false;
+
+        return GestureDetector(
+          onTap: () => _toggleTodo(index),
+          onLongPress: () => _
